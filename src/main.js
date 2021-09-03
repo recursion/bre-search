6935687c6d90bf9226da4ec2e86a283b30973b1a
Michael Symmes <msymmes@sitespect.com>
2021-09-02 20:32:12 -0400

    Add search button to global

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.svelte';
 const app = new App({
 	target: document.querySelector("#listings-nav"),
 	props: {
-		locations: '#sf6_advancedlocationtextsearch',
-		priceMin: '#sf6_min_price',
-		priceMax: '#sf6_max_price',
-		bedrooms: '#sf6_tmin_bedrooms',
-		bathrooms: '#sf6_tmin_bathrooms',
-		propertyType: "#sf6_multiple_property_type",
-		subPropertyType: "#sf6_feature_f_3009"
+		locations: '#sf5_advancedlocationtextsearch',
+		priceMin: '#sf5_min_price',
+		priceMax: '#sf5_max_price',
+		bedrooms: '#sf5_tmin_bedrooms',
+		bathrooms: '#sf5_tmin_bathrooms',
+		propertyType: "#sf5_select_property_type",
+		subPropertyType: "#sf5_feature_f_3009",
+		searchButton: '#wpl_total_results5'
 	}
 });
 
